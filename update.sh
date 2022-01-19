fcae32dfca49084ce196dbd25edcb326a2a8d8ff
zhangchunsheng <zhangchunsheng423@gmail.com>
2022-01-19 16:45:01 +0800

    1. update dev config

--- update.sh
+++ update.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 npm run build:prod
-
+rm -rf ../datax-web/datax-admin/src/main/resources/static/*
+cp -r dist/* ../datax-web/datax-admin/src/main/resources/static
+cd ../datax-web
+git add datax-admin
+git commit -a -m 'update'
+git push -u origin feature/sgcc
